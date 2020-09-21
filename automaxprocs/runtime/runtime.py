9a60a3d1ffedf8039342aed43c675caf4871f58e
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-09-21 19:15:49 +0800

    feat: add runtime.py

---
+++ automaxprocs/runtime/runtime.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# CPUQuotaUndefined is returned when CPU quota is undefined
+CPUQuotaUndefined = 0
+# CPUQuotaUsed is returned when a valid CPU quota can be used
+CPUQuotaUsed = 1
+# CPUQuotaMinUsed is return when CPU quota is smaller than the min value
+CPUQuotaMinUsed = 2
