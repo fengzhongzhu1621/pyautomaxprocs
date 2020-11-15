c9f7cc21c21c0cf0b1a73e0aeb43544b437f1a88
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-11-15 15:04:58 +0800

    fix: 修复get_max_procs()的默认值

--- automaxprocs/maxprocs.py
+++ automaxprocs/maxprocs.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import multiprocessing
-#from automaxprocs.runtime import cpu_quota_to_max_procs
+from automaxprocs.runtime import cpu_quota_to_max_procs
 
-print("test")
+
 def get_cpu_count():
     return multiprocessing.cpu_count()
 
 
 def get_max_procs():
     max_procs, status = cpu_quota_to_max_procs(1)
-    if status == -1:
+    if max_procs == -1:
         return get_cpu_count()
     return max_procs
